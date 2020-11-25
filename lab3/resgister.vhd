75685bef8b2c10888615ab6d1384842a0b727166
Alexandre425 <alexandre.d.rodrigues@tecnico.ulisboa.pt>
2020-11-25 00:09:11 +0000

    added register reset

--- lab3/resgister.vhd
+++ lab3/resgister.vhd
@@ -4,20 +4,22 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity reg is
     generic (
-        X : integer := 12
+        N : integer := 12
     );
     port(
-        clk : in std_logic;
-        D : in std_logic_vector (X-1 downto 0);
-        Q : out std_logic_vector (X-1 downto 0)
+        clk, reset: in std_logic;
+        D : in std_logic_vector (N-1 downto 0);
+        Q : out std_logic_vector (N-1 downto 0)
     );
 end reg;
 
 architecture behavioral of reg is
 begin
-    process (clk)
+    process (clk, rst)
     begin
-        if(clk'event and clk = '1') then
+        if reset = '1' then
+            Q <= (others => '0');
+        elsif clk'event and clk = '1' then
             Q <= D;
         end if;
     end process;
